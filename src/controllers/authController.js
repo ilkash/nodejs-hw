516b2b7a775ef091ffd27f13f778a049ca11e45b
ilkash <126604951+ilkash@users.noreply.github.com>
2025-11-01 19:52:47 +0200

    task2

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,6 +5,9 @@ import bcrypt from 'bcrypt';
 import { createSession, setSessionCookies } from '../services/auth.js';
 import jwt from 'jsonwebtoken';
 import { sendEmail } from '../utils/sendEmail.js';
+import handlebars from 'handlebars';
+import path from 'node:path';
+import fs from 'node:fs/promises';
 
 export const registerUser = async (req, res, next) => {
   const { email, password } = req.body;
@@ -80,27 +83,35 @@ export const requestResetEmail = async (req, res, next) => {
   const { email } = req.body;
 
   const user = await User.findOne({ email });
-  // Якщо користувача нема — навмисно повертаємо ту саму "успішну"
-  // відповідь без відправлення листа (anti user enumeration).
   if (!user) {
-    return res.status(200).json({
-      message: 'If this email exists, a reset link has been sent',
-    });
+    return next(createHttpError(404, 'User not found'));
   }
 
-  // Користувач є — генеруємо короткоживучий JWT і відправляємо лист
   const resetToken = jwt.sign(
     { sub: user._id, email },
     process.env.JWT_SECRET,
     { expiresIn: '15m' },
   );
 
+  // 1. Формуємо шлях до шаблона
+  const templatePath = path.resolve('src/templates/reset-password-email.html');
+  // 2. Читаємо шаблон
+  const templateSource = await fs.readFile(templatePath, 'utf-8');
+  // 3. Готуємо шаблон до заповнення
+  const template = handlebars.compile(templateSource);
+  // 4. Формуємо із шаблона HTML документ з динамічними даними
+  const html = template({
+    name: user.username,
+    link: `${process.env.FRONTEND_DOMAIN}/reset-password?token=${resetToken}`,
+  });
+
   try {
     await sendEmail({
       from: process.env.SMTP_FROM,
       to: email,
       subject: 'Reset your password',
-      html: `<p>Click <a href="${resetToken}">here</a> to reset your password!</p>`,
+      // 5. Передаємо HTML у функцію надписання пошти
+      html,
     });
   } catch {
     next(
@@ -108,11 +119,7 @@ export const requestResetEmail = async (req, res, next) => {
     );
     return;
   }
-
-  // Та сама "нейтральна" відповідь
-  res.status(200).json({
-    message: 'If this email exists, a reset link has been sent',
-  });
+  res.status(200).json({ message: 'Password reset email sent successfully' });
 };
 // export const requestResetEmail = async (req, res, next) => {
 //   const { email } = req.body;
